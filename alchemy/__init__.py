c50842e9b21f4de5984b16713a2c4afad49360ad
gabrioli <oliveiragabrielly.dev@gmail.com>
2026-05-05 20:43:04 -0300

    Adiciona o modulo recipe ao init do alchemy

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -1,5 +1,5 @@
 from .elements import create_air
 from .potions import healing_potion as heal
-from .transmutation import lead_to_gold
+from .transmutation import recipes
 
-__all__ = ["create_air", "heal"]
+__all__ = ["create_air", "heal", "recipes"]
